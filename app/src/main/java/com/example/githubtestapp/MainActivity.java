63c9e48a49fe64babd8610fc5b2c30f5a7839acc
f.atieh <fa===>
2019-12-10 10:47:11 +0200

    add doSomeWork method

--- app/src/main/java/com/example/githubtestapp/MainActivity.java
+++ app/src/main/java/com/example/githubtestapp/MainActivity.java
@@ -22,4 +22,16 @@ public class MainActivity extends AppCompatActivity {
 
         boolean isValid = true;
     }
+
+    private void doSomeWork(){
+        int i = 10;
+        int ii = 50;
+
+        String isString = Integer.toString(i);
+
+        if( i <= 100){
+            isString = Integer.toString(ii + 9);
+        }
+    }
+
 }
